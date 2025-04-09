3115c6b5afcf79f294d191f0328a40bb1002224f
Ves <vesmaybevesper@gmail.com>
2025-04-08 21:34:24 -0400

    Add files via upload

---
+++ src/client/java/vesper/fancywarpmenu/FancyWarpMenuClient.java
@@ -0,0 +1,10 @@
+package vesper.fancywarpmenu;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FancyWarpMenuClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
